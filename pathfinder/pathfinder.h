74c6612ce07f0d070140d03956fe5d85ceb65281
Andreas Valter <andreasvalter@gmail.com>
2014-10-10 14:37:40 +0200

    Working simple pathfinder working

--- pathfinder/pathfinder.h
+++ pathfinder/pathfinder.h
@@ -1,5 +1,62 @@
 #pragma once
+#include <vector>
+
 namespace pathfinder {
+struct vec2
+{
+	int x;
+	int y;
+	vec2(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	vec2() : x(0), y(0) {
+	}
+};
+
+struct node
+{
+	typedef std::vector<node>::const_iterator nodePtr;
+	vec2 pos;
+	unsigned int h; //Estimated cost to travel to goal
+	unsigned int g; //Cost to travel here
+	const nodePtr* parent;
+	node(const vec2* pPos, unsigned int nH, unsigned int nG, const nodePtr* pParent) : 
+		pos(pPos->x, pPos->y), h(nH), g(nG), parent(pParent) 
+	{}
+};
+
+class searchSpace
+{
+public:
+	searchSpace(const int nMapWidth, const int nMapHeight, const unsigned int* pMap) :
+		m_nMapWidth(nMapWidth), m_nMapHeight(nMapHeight), m_pMap(pMap) 
+	{}
+	bool addNeighbouringNodes();
+	bool insertInitialNodes(const vec2& nStartPos, const vec2& nGoalPos);
+	bool update();
+	std::vector<node> m_vNodeVector;
+	std::vector<node> m_visitedNodes;
+
+private:
+	const int m_nMapWidth;
+	const int m_nMapHeight;
+	const unsigned int* m_pMap;
+	vec2 m_start;
+	vec2 m_goal;
+	node::nodePtr m_activeNode;
+	
+	bool validateVec(const vec2& nPos);
+	bool insertIfValid(const node::nodePtr* pParent, const vec2& nPos, std::vector<pathfinder::node>& rNodeList);
+};
+
+
+unsigned int distNoSq(const vec2& sPosA, const vec2& sPosB);
+
+unsigned int distManhattan(const vec2& sPosA, const vec2& sPosB);
+
 int TestFunction(int i);
+
+int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize);
 
 } // namespace pathfinder
--- test/test.cpp
+++ test/test.cpp
@@ -4,16 +4,42 @@
 class PathfinderTester : public ::testing::Test {
 protected:
 	virtual void SetUp() {
-
+		nMapWidth = 3;
+		nMapHeight = 3;
+		sSearchSpace = new pathfinder::searchSpace(nMapWidth, nMapHeight, pMap);
 	}
 
 	virtual void TearDown() {
-
+		delete sSearchSpace;
 	}
-
+	unsigned int* pMap;
+	pathfinder::vec2 sStart;
+	pathfinder::vec2 sGoal;
+	int nMapWidth;
+	int nMapHeight;
+	pathfinder::searchSpace* sSearchSpace;
 };
 
-TEST(PathfinderTester, HelloTestWorld) {
-	bool b = (0 == pathfinder::TestFunction(0));
-	EXPECT_TRUE(b);
+TEST_F(PathfinderTester, ManhattanDistance)
+{
+	EXPECT_EQ(0, pathfinder::distManhattan(sStart, sGoal));
+	EXPECT_EQ(2, pathfinder::distManhattan(pathfinder::vec2(), pathfinder::vec2(1,1)));
+}
+
+TEST_F(PathfinderTester, AddNeighbouringNodes)
+{
+	pathfinder::node n(new pathfinder::vec2(1,1), 0, 0, nullptr);
+	pathfinder::vec2 start(1,1);
+	pathfinder::vec2 goal(2,0);
+	EXPECT_TRUE(sSearchSpace->insertInitialNodes(start, goal));
+	EXPECT_FALSE(sSearchSpace->update());
+	EXPECT_EQ(0, sSearchSpace->m_vNodeVector.size());
+	EXPECT_EQ(1, sSearchSpace->m_visitedNodes.size());
+	sSearchSpace->addNeighbouringNodes();
+	
+	EXPECT_EQ(4, sSearchSpace->m_vNodeVector.size());
+	EXPECT_FALSE(sSearchSpace->update());
+
+	sSearchSpace->addNeighbouringNodes();
+	EXPECT_TRUE(sSearchSpace->update());
 }
